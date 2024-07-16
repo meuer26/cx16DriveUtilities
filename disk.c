3d1c6cdad1dcce46058437e6962843dda3b24726
meuer26 <daniel.m.omalley@gmail.com>
2024-07-15 22:16:30 -0400

    Removed global variables

--- disk.c
+++ disk.c
@@ -3,86 +3,49 @@
 #include <stdio.h>
 #include "screen.h"
 
-unsigned char globalFile, globalDevice, globalChannel, globalDriveStatus, globalReturnedByte;
-char * globalCommand = "";
-
-void clearGlobalVars()
-{
-    globalFile = 0;
-    globalDevice = 0;
-    globalChannel = 0;
-    globalDriveStatus = 0;
-    globalReturnedByte = 0;
-    globalChannel = 0;
-}
-
 void printCommandChannel(unsigned char file, unsigned char device, unsigned char channel)
 {
-    clearGlobalVars();
-    
-    globalFile = file;
-    globalDevice = device;
-    globalChannel = channel;
+    unsigned char returnedByte;
 
-    cbm_k_setlfs(globalFile, globalDevice, globalChannel); 
+    cbm_k_setlfs(file, device, channel); 
     cbm_k_setnam("");
     cbm_k_open();
-    cbm_k_chkin(globalFile);
+    cbm_k_chkin(file);
 
-    globalReturnedByte = cbm_k_getin();
+    returnedByte = cbm_k_getin();
 
-    while (globalReturnedByte != 0xd)
+    while (returnedByte != 0xd)
     {
-        cbm_k_bsout(globalReturnedByte);
-        globalReturnedByte = cbm_k_getin();
+        cbm_k_bsout(returnedByte);
+        returnedByte = cbm_k_getin();
     }
-
 }
 
 unsigned char readDriveStatus(unsigned char file, unsigned char device, unsigned char channel)
 {
-    clearGlobalVars();
-    
-    globalFile = file;
-    globalDevice = device;
-    globalChannel = channel;
+    unsigned char driveStatus;
 
-    cbm_k_setlfs(globalFile, globalDevice, globalChannel); 
+    cbm_k_setlfs(file, device, channel); 
     cbm_k_setnam("");
     cbm_k_open();
-    cbm_k_ckout(globalFile);
-    globalDriveStatus = cbm_k_readst();
+    cbm_k_ckout(file);
+    driveStatus = cbm_k_readst();
 
-    return globalDriveStatus;
-
+    return driveStatus;
 }
 
 
 void sendDriveCommand(unsigned char file, unsigned char device, unsigned char channel, char * command)
 {
-    clearGlobalVars();
-
-    globalFile = file;
-    globalDevice = device;
-    globalChannel = channel;
-    globalCommand = command;
-
-    cbm_k_setlfs(globalFile, globalDevice, globalChannel);
-    cbm_k_setnam(globalCommand);
+    cbm_k_setlfs(file, device, channel);
+    cbm_k_setnam(command);
     cbm_k_open();
-
 }
 
 
 void loadCommand(unsigned char file, unsigned char device, unsigned char channel, char * fileName)
 {
-    clearGlobalVars();
-    
-    globalFile = file;
-    globalDevice = device;
-    globalChannel = channel;
-
-    cbm_k_setlfs(globalFile, globalDevice, globalChannel); 
+    cbm_k_setlfs(file, device, channel); 
     cbm_k_setnam(fileName);
     cbm_k_load(0, 0xc000);
 
